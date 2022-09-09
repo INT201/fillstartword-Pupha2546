971734daea095398a96ee0a0955551d634be3e82
Pupha <pupha.pomn@kmutt.ac.th>
2022-09-09 23:32:50 +0700

    test01

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,18 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  console.log("Hello")
+  if (startWord === null || word === null){
+    return undefined
+  }
+  else if (startWord === undefined || word === undefined) {
+    return undefined
+  }
+  else if (word.includes(startWord)) {
+    return word
+  }
+  else{
+    return startWord + word
+  }
 }
 module.exports = fillStartWord
+
